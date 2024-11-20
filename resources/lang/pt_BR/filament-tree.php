63eff1f2e60a1e8bffd1f774db2787d87e4fec87
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-11-20 09:18:26 -0300

    pt_BR support

---
+++ resources/lang/pt_BR/filament-tree.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'root' => 'Raiz',
+    
+    /*
+        |--------------------------------------------------------------------------
+        | Buttons
+        |--------------------------------------------------------------------------
+    */
+    'button.save' => 'Salvar',
+    'button.expand_all' => 'Expandir Todos',
+    'button.collapse_all' => 'Recolher Todos',
+
+    /*
+        |--------------------------------------------------------------------------
+        | Form
+        |--------------------------------------------------------------------------
+    */
+    'components.tree.buttons.select_all.label' => 'Selecionar Todos',
+    'components.tree.buttons.deselect_all.label' => 'Desmarcar Todos',
+    'components.tree.buttons.expand_all.label' => 'Expandir Todos',
+    'components.tree.buttons.collapse_all.label' => 'Recolher Todos',
+    
+    /*
+        |--------------------------------------------------------------------------
+        | Message
+        |--------------------------------------------------------------------------
+    */
+    'actions.delete.confirmation.with_children' => 'Tem certeza de que deseja excluir este registro e seus filhos?',
+];
